afa6644f9c33dd20bee7b87b93699ed429edadc2
Cal Corbin <calcorbin@gmail.com>
2021-10-22 11:57:07 -0500

    Add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 import logo from './logo.svg';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache()
+});
+
+const data = async () =>{
+    await client
+        .query({
+          query: gql`
+      query GetRates {
+        rates(currency: "USD") {
+          currency
+        }
+      }
+    `
+        })
+        .then(result => console.log(result));}
 
 function App() {
   return (
